55d4c3cbfbe27ca4a3b357b97f5fb2fba008f68b
pascall de creator <pascall.de.creator@gmail.com>
2022-01-13 12:16:06 +0000

    Initial v1.0.0

---
+++ script.js
@@ -0,0 +1,25 @@
+var memeImageEl, titleEl, authorEl
+memeImageEl = document.getElementById('memeImage')
+titleEl = document.getElementById('title')
+authorEl = document.getElementById('author')
+
+function loadMeme(){
+  fetch('https://meme-api.herokuapp.com/gimme')
+      .then(jsonData => jsonData.json())
+      .then(data => response(data))
+
+  let response = (data) => {
+    console.log(data)
+    var authorOfMeme, title, imageUrl, postLink, ups;
+    authorOfMeme = data.author
+    title = data.title
+    imageUrl = data.url
+    postLink = data.postLink
+    ups = data.ups
+    console.log(postLink)
+    
+    memeImageEl.style.backgroundImage = "url('"+imageUrl+"')";
+    titleEl.innerText = title
+    titleEl.href=postLink; 
+    authorEl.innerText = "By " + authorOfMeme
+}}
